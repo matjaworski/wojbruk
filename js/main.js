5678933af97ec448ddc0714b56aa72e9c8b8489d
matijaworski <jwrdamillionaire@gmail.com>
2021-03-09 21:35:06 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const nav = document.querySelector('.navbar')
+
+    function addShadow() {
+        if (window.scrollY >= 300) {
+            nav.classList.add('shadow-bg')
+        } else {
+            nav.classList.remove('shadow-bg')
+        }
+    }
+
+    window.addEventListener('scroll', addShadow)
+
+
+
+
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+
+
+
+    $('.your-class').slick({
+        // setting- name: setting - value
+        });
+
+
+
+});
